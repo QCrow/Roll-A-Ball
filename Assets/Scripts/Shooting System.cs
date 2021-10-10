e09f83e97302beb37925580fc7fe0c830e495143
Steven Tian <90989589+StevenTian0@users.noreply.github.com>
2021-10-09 22:12:55 -0400

    turret

---
+++ Assets/Scripts/Shooting System.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingSystem : MonoBehaviour
+{
+    public float fireRate;
+    public float fieldOfView;
+    public bool beam;
+    public GameObject projectile;
+    public List<GameObject> projectileSpawns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Trackingsystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trackingsystem : MonoBehaviour
+{
+
+    public float speed = 8.0f;
+    public GameObject m_target = null;
+    Vector3 m_lastKnownPosition = Vector3.zero;
+    Quaternion m_lookAtRotation;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_target)
+        {
+            if (m_lastKnownPosition != m_target.transform.position)
+            {
+                m_lastKnownPosition = m_target.transform.position;
+                m_lookAtRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position);
+
+            }
+            if (transform.rotation != m_lookAtRotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, m_lookAtRotation, speed * Time.deltaTime);
+            }
+        }
+
+
+
+    }
+    bool SetTarget(GameObject target)
+    {
+        if (target)
+        {
+            return false;
+        }
+        m_target = target;
+
+        return true;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
